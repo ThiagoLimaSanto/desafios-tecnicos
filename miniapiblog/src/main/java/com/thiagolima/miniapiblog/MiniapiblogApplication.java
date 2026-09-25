f0a0e4846bc7321986636507d2bf90ee091ab171
Thiago Lima Santos <tlima7601@gmail.com>
2026-09-25 12:24:45 -0300

    Novo desafio: Mini Api de Blog

---
+++ miniapiblog/src/main/java/com/thiagolima/miniapiblog/MiniapiblogApplication.java
@@ -0,0 +1,13 @@
+package com.thiagolima.miniapiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniapiblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniapiblogApplication.class, args);
+	}
+
+}
---
+++ miniapiblog/src/test/java/com/thiagolima/miniapiblog/MiniapiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagolima.miniapiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniapiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
